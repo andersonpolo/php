32211f548cd73ceed374d28a41c79b62617bc6bd
andersonpolo <andersonpolodefaria@gmail.com>
2018-06-15 03:35:45 -0300

    Nova pasta String com exemplos

---
+++ String/exemplo01.php
@@ -0,0 +1,21 @@
+<?php 
+
+$nome = "Hcode";
+
+$nome2 = 'treinamentos';
+
+var_dump($nome, $nome2);
+
+echo "<br>";
+
+// no echo ao usar ("") você imprime o valor da variavel na saida.
+
+echo "ABC $nome";
+
+echo "<br>";
+
+// mas quando se usa ('') o echo coloca na saída como se fosse tudo texto, sem verificar.
+
+echo 'ABC $nome';
+
+ ?>
