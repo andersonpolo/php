eb86ef77f61e9fdd1f1ea17c79010cf98db97303
andersonpolo <andersonpolodefaria@gmail.com>
2018-06-23 02:59:38 -0300

    criação da pasta for e varios exemplos php

---
+++ for/exemplo01.php
@@ -0,0 +1,11 @@
+<?php 
+
+for($i = 0; $i <= 100; $i++){
+
+	if($i % 3 === 0) echo $i . " - Te amo muito mesmo gata!!<br>";
+
+	if($i % 3 !== 0)echo $i . " - Eu te amo Ruti S2! Você é muito gata!!! <br> ";
+}
+
+
+ ?>
